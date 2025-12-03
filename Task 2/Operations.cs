f93867d6c52190034fd594dcd9802f5c9e61ddd1
bibek0-0 <luitelbibek0@gmail.com>
2025-12-03 11:41:44 +0545

    Task 2

---
+++ Task 2/Operations.cs
@@ -0,0 +1,15 @@
+﻿public delegate int Calculate(int a, int b);
+public delegate double DiscountStrategy(double price);
+
+public class Operations
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+}
+
+public class Discounts
+{
+    public double FestivalDiscount(double price) => price * 0.80;
+    public double SeasonalDiscount(double price) => price * 0.90;
+    public double NoDiscount(double price) => price;
+}
--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,34 +1,35 @@
-﻿public delegate int Calculate(int a, int b);
-public delegate double DiscountStrategy(double price);
-
-class Operations
+﻿class Program
 {
-    public int Add(int a, int b) => a + b;
-    public int Subtract(int a, int b) => a - b;
-}
+    public static double CalculateFinalPrice(double originalPrice, DiscountStrategy strategy)
+    {
+        return strategy(originalPrice);
+    }
 
-class Discounts
-{
-    public double FestivalDiscount(double price) => price * 0.80;
-    public double SeasonalDiscount(double price) => price * 0.90;
-    public double NoDiscount(double price) => price;
-}
-
-class Program
-{
     static void Main()
     {
+        Console.WriteLine("TASK 2:Delegate");
+
         Operations ops = new Operations();
 
+        // Using Calculate delegate
         Calculate calc = ops.Add;
         Console.WriteLine("Add: " + calc(10, 5));
 
         calc = ops.Subtract;
         Console.WriteLine("Subtract: " + calc(10, 5));
 
+        // Discount strategy
         Discounts d = new Discounts();
-        Console.WriteLine("Festival: " + d.FestivalDiscount(1000));
-        Console.WriteLine("Seasonal: " + d.SeasonalDiscount(1000));
-        Console.WriteLine("No Discount: " + d.NoDiscount(1000));
+
+
+
+        Console.WriteLine("\n TASK 2.1: Discount Strategy");
+
+        Console.WriteLine("Final (Festival): " + CalculateFinalPrice(1000, d.FestivalDiscount));
+        Console.WriteLine("Final (Seasonal): " + CalculateFinalPrice(1000, d.SeasonalDiscount));
+        Console.WriteLine("Final (No Discount): " + CalculateFinalPrice(1000, d.NoDiscount));
+
+        // Lambda expression for 30% discount
+        Console.WriteLine("Final (Lambda 30% Off): " + CalculateFinalPrice(1000, p => p * 0.70));
     }
 }
