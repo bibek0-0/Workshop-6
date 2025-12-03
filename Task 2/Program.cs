43ff51d2cb00e15c749301a42f034958108dea5e
bibek0-0 <luitelbibek0@gmail.com>
2025-12-03 11:28:44 +0545

    Task 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Task_2
+﻿public delegate int Calculate(int a, int b);
+public delegate double DiscountStrategy(double price);
+
+class Operations
 {
-    internal class Program
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+}
+
+class Discounts
+{
+    public double FestivalDiscount(double price) => price * 0.80;
+    public double SeasonalDiscount(double price) => price * 0.90;
+    public double NoDiscount(double price) => price;
+}
+
+class Program
+{
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Operations ops = new Operations();
+
+        Calculate calc = ops.Add;
+        Console.WriteLine("Add: " + calc(10, 5));
+
+        calc = ops.Subtract;
+        Console.WriteLine("Subtract: " + calc(10, 5));
+
+        Discounts d = new Discounts();
+        Console.WriteLine("Festival: " + d.FestivalDiscount(1000));
+        Console.WriteLine("Seasonal: " + d.SeasonalDiscount(1000));
+        Console.WriteLine("No Discount: " + d.NoDiscount(1000));
     }
 }
