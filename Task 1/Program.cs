d25ed7363f99695a1f44148d278733388330bc41
bibek0-0 <luitelbibek0@gmail.com>
2025-12-03 11:25:22 +0545

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿class Rectangle
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    public double Area => Length * Width;
+    public double GetArea() => Length * Width;
+    public double GetPerimeter() => 2 * (Length + Width);
+}
+
+class Program
+{
+    static void Main()
+    {
+        Rectangle rect = new Rectangle { Length = 10, Width = 5 };
+
+        Console.WriteLine("Area: " + rect.Area);
+        Console.WriteLine("Perimeter: " + rect.GetPerimeter());
+    }
+}
